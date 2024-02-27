154ba843ca8b653076fe8de252fd18e290aecaa1
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-02-27 23:55:09 +0100

    ssl

--- api/server.js
+++ api/server.js
@@ -1,5 +1,11 @@
-const http = require('http');
+const https = require('https');
 const app = require('./app');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/cyril-develop.fr/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/cyril-develop.fr/fullchain.pem'),
+};
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -33,7 +39,7 @@ const errorHandler = error => {
     }
 };
 
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
