670a069e841c8f7fdb5c62bc3da1dbfc2007e018
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-03-27 17:00:38 +0100

    modif anim

--- client/script/animation/heroSection.js
+++ client/script/animation/heroSection.js
@@ -1,73 +1,60 @@
 export const initHero = () => {
-    const tlHero = gsap.timeline();
+  const tlHero = gsap.timeline();
 
-    const letterElements = document.querySelectorAll(".letter_fadeIn");
-    const shuffledLetters = Array.from(letterElements).sort(
-        () => Math.random() - 0.5
+  const blocs = document.querySelectorAll(".bloc_fadeOut");
+  const shuffledBlocs = Array.from(blocs).sort(() => Math.random() - 0.6);
+  const letterElements = document.querySelectorAll(".letter_fadeIn");
+  const shuffledLetters = Array.from(letterElements).sort(
+    () => Math.random() - 0.6
+  );
+
+  tlHero
+    .to(shuffledLetters, {
+      autoAlpha: 1,
+      duration: 0.2,
+      stagger: 0.02,
+    })
+    .to(shuffledBlocs, {
+      autoAlpha: 0,
+      duration: 0.2,
+      stagger: 0.02,
+    })
+    .addLabel("letters")
+    .to(
+      [".fadeIn_bottom", ".hero_footer"],
+      {
+        autoAlpha: 1,
+        y: 0,
+        duration: 0.2,
+        stagger: 0,
+      },
+      "letters"
     );
 
-    tlHero
-        .to(shuffledLetters, {
-            autoAlpha: 1,
-            duration: 0.5,
-            stagger: 0.04,
-        })
-        .addLabel("letters")
-        .to(
-            [".fadeIn_bottom", ".hero_footer"],
-            {
-                autoAlpha: 1,
-                y: 0,
-                delay: 0,
-                stagger: 0,
-            },
-            "letters"
-        )   
-    if (window.innerWidth < 700) {
-        tlHero.to(".hero_main_img", {
-            x: "0",
-            y: "0",
-            rotate: "0",
-            duration: 0.3,
-        });
-    } else if (window.innerWidth < 1200) {
-        tlHero.to(".hero_main_img", {
-            x: "50%",
-            y: "10%",
-            rotate: "-4deg",
-            duration: 0.3,
-        });
-    }
-    else if (window.innerWidth > 1200)  {
-        tlHero.to(".hero_main_img", {
-            x: "85%",
-            y: "10%",
-            rotate: "-4deg",
-            duration: 0.3,
-        });
-    }
-    tlHero
-        .to(".line", {
-            visibility: "visible",
-            width: "15rem",
-        })
-        .to(".hero_footer_scroll", {
-            autoAlpha: 1,
-        });
+  tlHero
+    .to(".line", {
+      visibility: "visible",
+      width: "15rem",
+      duration: 0.2,
+    })
+    .to(".hero_footer_scroll", {
+      autoAlpha: 1,
+      duration: 0.2,
+    });
 
-    const tlScroll = gsap.timeline({ repeat: -1 });
+  const tlScroll = gsap.timeline({ repeat: -1 });
 
-    tlScroll
-        .to(".hero_footer_scroll_anim", {
-            duration: 0.5,
-            y: 10,
-            opacity: 1,
-            ease: "power2.inOut",
-        })
-        .to(".hero_footer_scroll_anim", {
-            duration: 1,
-            y: 0,
-            opacity: 0,
-            ease: "power2.inOut",
-        });
+  tlScroll
+    .to(".hero_footer_scroll_anim", {
+      duration: 0.2,
+      y: 10,
+      opacity: 1,
+      ease: "power2.inOut",
+    })
+    .to(".hero_footer_scroll_anim", {
+      duration: 0.6,
+      y: 0,
+      opacity: 0,
+      ease: "power2.inOut",
+    });
 };
--- client/script/animation/menu.js
+++ client/script/animation/menu.js
@@ -2,62 +2,63 @@
 // -------- Display and animate menu -------- //
 /*********************************************/
 export const AnimMenu = () => {
-    const btnHamb = document.querySelectorAll(".btn_menu");
-    const navWrapper = document.querySelector(".hero_header_menu");
-    const allLink = document.querySelectorAll('.nav_list_item');
+  const btnHamb = document.querySelectorAll(".btn_menu");
+  const navWrapper = document.querySelector(".hero_header_menu");
+  const allLink = document.querySelectorAll(".nav_list_item");
 
-    function resetMenu() {
-        navWrapper.classList.remove("active");
-        tlMenu.reverse();
-    };
+  function resetMenu() {
+    navWrapper.classList.remove("active");
+    tlMenu.reverse();
+  }
 
-    const tlMenu = gsap.timeline({ paused: true });
+  const tlMenu = gsap.timeline({ paused: true });
 
-    btnHamb.forEach((btn) => btn.addEventListener("click", toggleMenu));
-    allLink.forEach(link => link.addEventListener('click', navigateToSection));
+  btnHamb.forEach((btn) => btn.addEventListener("click", toggleMenu));
+  allLink.forEach((link) => link.addEventListener("click", navigateToSection));
 
-    tlMenu
-        .fromTo(
-            ".btn_menu_open",
-            { y: "0" },
-            { y: "-100%", duration: 0.2, delay: 0 }
-        )
-        .to(navWrapper, { y: "0", duration: 0.2, delay: 0, ease: "none" })
-        .fromTo(
-            ".btn_menu_close",
-            { y: "-100%" },
-            { y: "0", duration: 0.1, delay: 0 }
-        )
-        .to(".nav_list_item_link", { y: 0, delay: 0, stagger: 0.2 });
+  tlMenu
+    .fromTo(
+      ".btn_menu_open",
+      { y: "0" },
+      { y: "-100%", duration: 0.1, delay: 0 }
+    )
+    .to(navWrapper, { y: "0", duration: 0.1, delay: 0, ease: "none" })
+    .fromTo(
+      ".btn_menu_close",
+      { y: "-100%" },
+      { y: "0", duration: 0.1, delay: 0 }
+    )
+    .to(".nav_list_item_link", { y: 0, delay: 0, stagger: 0.1 });
 
-    function toggleMenu() {
-        navWrapper.classList.toggle("active");
+  function toggleMenu() {
+    navWrapper.classList.toggle("active");
 
-        navWrapper.classList.contains("active") ? tlMenu.play() : tlMenu.reverse();
-    };
+    navWrapper.classList.contains("active") ? tlMenu.play() : tlMenu.reverse();
+  }
 
-    /************************************************************/
-    // -------- Animation when clicking on menu links -------- //
-    /**********************************************************/
-    function navigateToSection(){
-        resetMenu();
-        gsap.timeline({ ease: "power4.inOut" })
-        .set(".curtain", { flexDirection: "row" })
-        .set(".curtain_tile", { height: "0", width: "100%", top: "0" })
-        .to(".curtain_tile", {
-            duration: 0.75,
-            height: "100%",
-            top: "0%",
-            delay: 0.2,
-            stagger: 0.05,
-        })
-        .to(".curtain_tile", {
-            duration: 0.75,
-            height: "100%",
-            top: "100%",
-            delay: 0.2,
-            stagger: -0.05,
-        })
-        .set(".curtain_tile", { top: "0", height: "0" })
-    };
+  /************************************************************/
+  // -------- Animation when clicking on menu links -------- //
+  /**********************************************************/
+  function navigateToSection() {
+    resetMenu();
+    gsap
+      .timeline({ ease: "power4.inOut" })
+      .set(".curtain", { flexDirection: "row" })
+      .set(".curtain_tile", { height: "0", width: "100%", top: "0" })
+      .to(".curtain_tile", {
+        duration: 0.55,
+        height: "100%",
+        top: "0%",
+        delay: 0.1,
+        stagger: 0.05,
+      })
+      .to(".curtain_tile", {
+        duration: 0.55,
+        height: "100%",
+        top: "100%",
+        delay: 0.1,
+        stagger: -0.05,
+      })
+      .set(".curtain_tile", { top: "0", height: "0" });
+  }
 };
